19bcdb07d8954b31c709388779e943589c9dd7c1
sergio aranda lizano <s.arandal@alumnos.upm.es>
2020-12-29 12:30:42 +0100

    Erased old commentary

--- ProjectFindFood.py
+++ ProjectFindFood.py
@@ -17,17 +17,17 @@ def findFood(nombre):
 
 def comparePicture(x,nombre):
  BasePictures = os.listdir('D:/APUNTES/Python/Alimentos')
- im = cv2.imread(BasePictures[x],cv2.IMREAD_GRAYSCALE) # imagen a , aqui añadir la imagen que se clicka en la lista , cambiaria el BasePictures
+ im = cv2.imread(BasePictures[x],cv2.IMREAD_GRAYSCALE)
  y = 0
  encontrado = False
  while y < len(BasePictures) and (encontrado == False):
-  im2 = cv2.imread(BasePictures[y],cv2.IMREAD_GRAYSCALE)  # imagen b , aqui se tiene que hacer un bucle iterando en las imagenes base que tenemos para comprobar que objeto es . Seria un while(!encontrado && quedanObjetos) se usa el matchShape
+  im2 = cv2.imread(BasePictures[y],cv2.IMREAD_GRAYSCALE)
   d1 = cv2.matchShapes(im, im2, cv2.CONTOURS_MATCH_I2, 0)  # compara ambas imagenes
   if d1 <= 0.01:  # si el numero es menor a 0.01 es el mismo objeto, normalmente no se llega a 0.01 de desviación al modificar longitud, anchura o cantidad en los objetos.
     encontrado = True
     fecha = datetime.now()
     guardarDatos(fecha,nombre)
-    # añadir el objeto con su nombre y la fecha en la lista de la otra UI
+
 
 def guardarDatos(fecha,nombre):
     archivo = open('D:/APUNTES/Python/lista.txt',"a")
